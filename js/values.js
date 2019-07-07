d770c6c17bf1bcf15f92d25ce2313f4d5a391cd0
Claudio <claum0395@gmail.com>
2019-07-06 22:17:35 -0500

    se agrega la funcionalidad

---
+++ js/values.js
@@ -0,0 +1,6 @@
+if (!sessionStorage.getItem('logOn')) {
+    sessionStorage.setItem('nombreGrup', 'true')
+    sessionStorage.setItem('apellidoGrup', 'true')
+    sessionStorage.setItem('logOn', 'true')
+    console.log("entra a los values")
+}
