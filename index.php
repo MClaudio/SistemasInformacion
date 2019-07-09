9f10920eff87649f7a14417751dd91289b31af70
Mauricio <mauricio@MacBook-Pro-de-Mauricio.local>
2019-07-09 00:47:26 -0500

    bjhbj

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.html">Registro<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.php">Registro<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="public/config.html">Configuarar formulario</a>
